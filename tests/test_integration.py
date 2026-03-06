95eb03220b3fb93b223ce2432d0d7edb995085e4
Łukasz Kułacz <lukasz.kulacz@rimedolabs.com>
2026-03-06 11:29:19 +0100

    Dodanie kilku testów
    Dodanie pierwszych testów jednostkowych i integracyjnych.

---
+++ tests/test_integration.py
@@ -0,0 +1,16 @@
+from src.models import Apartment
+from src.manager import Manager
+from src.models import Parameters
+
+
+def test_load_data():
+    parameters = Parameters()
+    manager = Manager(parameters)
+    assert isinstance(manager.apartments, dict)
+    assert isinstance(manager.tenants, dict)
+    assert isinstance(manager.transfers, list)
+    assert isinstance(manager.bills, list)
+
+    for apartment_key, apartment in manager.apartments.items():
+        assert isinstance(apartment, Apartment)
+        assert apartment.key == apartment_key
---
+++ tests/test_unit.py
@@ -0,0 +1,45 @@
+import pytest
+
+from pydantic import ValidationError
+from src.models import Apartment
+
+
+def test_apartment_fields():
+    data = Apartment(
+        key="apart-test",
+        name="Test Apartment",
+        location="Test Location",
+        area_m2=50.0,
+        rooms={
+            "room-1": {"name": "Living Room", "area_m2": 30.0},
+            "room-2": {"name": "Bedroom", "area_m2": 20.0}
+        }
+    )
+    assert data.key == "apart-test"
+    assert data.name == "Test Apartment"
+    assert data.location == "Test Location"
+    assert data.area_m2 == 50.0
+    assert len(data.rooms) == 2
+
+
+def test_apartment_from_dict():
+    data = {
+        "key": "apart-test",
+        "name": "Test Apartment",
+        "location": "Test Location",
+        "area_m2": 50.0,
+        "rooms": {
+            "room-1": {"name": "Living Room", "area_m2": 30.0},
+            "room-2": {"name": "Bedroom", "area_m2": 20.0}
+        }
+    }
+    apartment = Apartment(**data)
+    assert apartment.key == data["key"]
+    assert apartment.name == data["name"]
+    assert apartment.location == data["location"]
+    assert apartment.area_m2 == data["area_m2"]
+    assert len(apartment.rooms) == len(data["rooms"])
+
+    data['area_m2'] = "25m2" # Invalid field
+    with pytest.raises(ValidationError):
+        wrong_apartment = Apartment(**data)
